150987aa27b335e0a063d1d4944993b1556187ed
Vinícius de F. Marques <vini.type@gmail.com>
2020-04-24 02:45:13 +0000

    feat(cli/generator): generate model CRUD service

--- lib/cli/commands/generate/model.ts
+++ lib/cli/commands/generate/model.ts
@@ -6,11 +6,17 @@ export const command = 'model <name> [properties...]'
 export const aliases = ['mo']
 export const desc = 'Generate a new model with its properties'
 export const builder = (yargs: Argv<{}>) => yargs
+    .option('service', {
+        alias: 's',
+        type: 'boolean',
+        description: 'generate a basic CRUD model service'
+    })
     .positional('name', {
         describe: 'model name',
         type: 'string'
     })
     .positional('properties', {
-        describe: 'model properties (format: <name>:<type> or <name>?:<type>)'
+        // eslint-disable-next-line max-len
+        describe: 'model properties (format: <name>:<type> or <name>?:<type> for nullable)'
     })
 export const handler = generateModel
